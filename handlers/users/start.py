b413339fd2b4eec3a3e8016f2be90643c8b39a2d
Umidjon <mrumidjon2020@gmail.com>
2022-11-26 05:28:02 +0500

    start upadte

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -1,9 +1,12 @@
 from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandStart
 
+from keyboards.default.menu_keyboard import menu
+
 from loader import dp
 
 
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
     await message.answer(f"Salom, {message.from_user.full_name}!")
+    await message.answer("Menu", reply_markup=menu)
