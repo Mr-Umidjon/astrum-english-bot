eba61b1c7db409e8523840d6b94a5c2aca1a7924
Umidjon <mrumidjon2020@gmail.com>
2022-11-26 05:30:58 +0500

    menu keyboard

--- keyboards/default/menu_keyboard.py
+++ keyboards/default/menu_keyboard.py
@@ -1,1 +1,15 @@
-from
+from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
+
+menu = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text="📕 Headway Student's book Audios"),
+            KeyboardButton(text='📘 Headway Workbook Audios'),
+        ],
+        [
+            KeyboardButton(text="📙 Navigate Student's book Audios"),
+            KeyboardButton(text='📗 Navigate Workbook Audios'),
+        ],
+    ],
+    resize_keyboard=True
+)
