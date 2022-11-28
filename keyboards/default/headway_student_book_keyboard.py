762c2803ad42c6d8a187ffdfa1f9f6406d6208a6
Umidjon <mrumidjon2020@gmail.com>
2022-11-28 13:36:23 +0500

    new keyboards added

--- keyboards/default/headway_student_book_keyboard.py
+++ keyboards/default/headway_student_book_keyboard.py
@@ -20,3 +20,14 @@ menu_headway_student_book = ReplyKeyboardMarkup(
     ],
     resize_keyboard=True
 )
+
+levels = ['menu_hsbb_units', 'menu_hsbe_units', 'menu_hsbp_units', 'menu_hsbi_units', 'menu_hsbu_units',
+          'menu_hsba_units']
+
+menu_headway_student_book_levels = dict()
+for level in levels:
+    menu_level = ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
+    for i in range(1, 13, 2):
+        menu_level.add(KeyboardButton(text=f'unit {i}'), KeyboardButton(text=f'unit {i + 1}'))
+    menu_level.add(KeyboardButton(text='Go Back'))
+    menu_headway_student_book_levels[level] = menu_level
--- keyboards/default/headway_workbook_keyboard.py
+++ keyboards/default/headway_workbook_keyboard.py
@@ -20,3 +20,14 @@ menu_headway_workbook = ReplyKeyboardMarkup(
     ],
     resize_keyboard=True
 )
+
+levels = ['menu_hwbb_units', 'menu_hwbe_units', 'menu_hwbp_units', 'menu_hwbi_units', 'menu_hwbu_units',
+          'menu_hwba_units']
+
+menu_headway_work_book_levels = dict()
+for level in levels:
+    menu_level = ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
+    for i in range(1, 13, 2):
+        menu_level.add(KeyboardButton(text=f'unit {i}'), KeyboardButton(text=f'unit {i + 1}'))
+    menu_level.add(KeyboardButton(text='Go Back'))
+    menu_headway_work_book_levels[level] = menu_level
