941252e61a2b9907be367f9ce536a6fd39f2a99f
Umidjon <mrumidjon2020@gmail.com>
2022-12-16 20:35:46 +0500

    bot

--- data/config.py
+++ data/config.py
@@ -1,6 +1,16 @@
+# from environs import Env
+#
+# # environs kutubxonasidan foydalanish
+# env = Env()
+# env.read_env()
+#
+# # .env fayl ichidan quyidagilarni o'qiymiz
+# BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
+# ADMINS = env.list("ADMINS")  # adminlar ro'yxati
+# IP = env.str("ip")  # Xosting ip manzili
+
 import os
 
-# .env fayl ichidan quyidagilarni o'qiymiz
-BOT_TOKEN = str(os.environ.get('BOT_TOKEN'))  # Bot token
-ADMINS = list(os.environ.get('ADMINS'))  # adminlar ro'yxati
-IP = str(os.environ.get('ip'))  # Hosting ip manzili
+BOT_TOKEN = str(os.environ.get("BOT_TOKEN"))
+ADMINS = list(os.environ.get("ADMINS"))
+IP = str(os.environ.get("ip"))
