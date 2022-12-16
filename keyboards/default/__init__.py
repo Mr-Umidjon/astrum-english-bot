3ef775503c06e28e90e8be53b6373203c6f4e112
Umidjon <mrumidjon2020@gmail.com>
2022-12-16 21:31:59 +0500

    bot

--- keyboards/default/__init__.py
+++ keyboards/default/__init__.py
@@ -1,5 +1,1 @@
 from . import menu_keyboard
-from . import headway_student_book_keyboard
-from . import headway_workbook_keyboard
-from . import navigate_student_book_keyboard
-from . import navigate_workbook_keyboard
